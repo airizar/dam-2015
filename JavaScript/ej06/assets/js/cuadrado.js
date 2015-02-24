3772e87cf84d4bcd296ec8b93151fdafb7b5ab5b
airizar <airizar@gmail.com>
2015-02-24 17:50:16 +0100

    Ejercicio JavaScript herencias

---
+++ JavaScript/ej06/assets/js/cuadrado.js
@@ -0,0 +1,32 @@
+var App = App || { Model : { } };
+App.Model.Cuadrado=(function(){
+
+	var _alto;
+
+	function Cuadrado(x,y,alto){
+		//java:super(x,y):Ejecuta el constructor figura como sifueras yo
+		//App.Model.Figura.bind(this)(x,y)
+		//App.Model.Figura.apply(this,[x,y]}
+		App.Model.Figura.call(this,x,y);
+		_alto=(typeof alto ==='number') ? alto : 0;
+	}
+	//Indicamos el padre
+	Cuadrado.prototype=Object.create(App.Model.Figura.prototype);
+	Cuadrado.prototype.constructor=Cuadrado;
+	
+	
+	Cuadrado.prototype.getAlto=function(){
+		return _alto;
+	};
+	
+	Cuadrado.prototype.setAlto=function(alto){
+		_alto=(typeof alto ==='number') ? alto : _alto;
+		
+	};
+	Cuadrado.prototype.getArea=function(){
+		return Math.pow(_alto,2);
+		
+	};
+	
+	return Cuadrado;
+})();
---
+++ JavaScript/ej06/assets/js/rectangulo.js
@@ -0,0 +1,42 @@
+var App = App || { Model : { } };
+App.Model.Rectangulo=(function(){
+
+	var _ancho;
+	Cuadrado=App.Model.Cuadrado;
+	function Rectangulo(x,y,alto,ancho){
+		//java:super(x,y):Ejecuta el constructor figura como sifueras yo
+		//App.Model.Figura.bind(this)(x,y)
+		//App.Model.Figura.apply(this,[x,y]}
+		App.Model.Cuadrado.call(this,alto,x,y);
+		_ancho=(typeof ancho ==='number') ? ancho : 0;
+	}
+	//Indicamos el padre
+	Rectangulo.prototype=Object.create(App.Model.Cuadrado.prototype);
+	Rectangulo.prototype.constructor=Rectangulo;
+	
+	
+	Rectangulo.prototype.getAncho=function(){
+		return _ancho;
+	};
+	
+	Rectangulo.prototype.setAncho=function(ancho){
+		_ancho=(typeof ancho ==='number') ? ancho : _ancho;
+		
+	};
+	
+	Rectangulo.prototype.getArea=function(){
+		//this.getAlto():como el rectangulo no tiene el metodo getAlto,
+		//ira a su prototipo a buscar la funcion hasta encontrarla
+		return _ancho*this.getAlto();
+		
+	};
+	Rectangulo.prototype.getAreaCuadrado=function(){
+		//this.getAlto():como el rectangulo no tiene el metodo getAlto,
+		//ira a su prototipo a buscar la funcion hasta encontrarla
+		return Cuadrado.prototype.getArea.call(this);
+		
+	};
+	
+	
+	return Rectangulo;
+})();
